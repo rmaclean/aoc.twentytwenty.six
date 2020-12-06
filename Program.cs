2a28e89381f2981759befc4613df5eae7579785d
Robert MacLean <robert@sadev.co.za>
2020-12-06 10:28:49 +0200

    part 1

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+var fileRecords = new List<String>();
+var fileData = await File.ReadAllLinesAsync("data.txt");
+var currentGroup = "";
+foreach (var line in fileData)
+{
+    if (!string.IsNullOrWhiteSpace(line)) {
+        currentGroup += line.Trim();
+    } else {
+        fileRecords.Add(currentGroup);
+        currentGroup = "";
+    }
+}
+
+fileRecords.Add(currentGroup);
+
+var result = fileRecords
+    .Select(line => line.Distinct().Count())
+    .Sum();
+
+Console.WriteLine(result);
