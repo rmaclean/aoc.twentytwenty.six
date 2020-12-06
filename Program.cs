80005bd9198e4cfaed04f2ee810f07677a71cb46
Robert MacLean <robert@sadev.co.za>
2020-12-06 11:07:38 +0200

    part 2

--- Program.cs
+++ Program.cs
@@ -4,23 +4,40 @@ using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 
-var fileRecords = new List<String>();
+var groups = new List<List<String>>();
 var fileData = await File.ReadAllLinesAsync("data.txt");
-var currentGroup = "";
+var currentGroup = new List<String>();
 foreach (var line in fileData)
 {
-    if (!string.IsNullOrWhiteSpace(line)) {
-        currentGroup += line.Trim();
-    } else {
-        fileRecords.Add(currentGroup);
-        currentGroup = "";
+    if (!string.IsNullOrWhiteSpace(line))
+    {
+        currentGroup.Add(line.Trim());
+    }
+    else
+    {
+        groups.Add(currentGroup);
+        currentGroup = new List<String>();
     }
 }
 
-fileRecords.Add(currentGroup);
+groups.Add(currentGroup);
 
-var result = fileRecords
-    .Select(line => line.Distinct().Count())
-    .Sum();
+var result = groups.Select(group => {
+    var everyoneAnsweredYesTo = group[0];
+    for (var index = 1; index < group.Count; index++)
+    {
+        var person = group[index];
+        var intesection = person.Intersect(everyoneAnsweredYesTo).ToArray();
+        if (intesection.Length > 0) 
+        {
+            everyoneAnsweredYesTo = new string(intesection);
+        } else {
+            everyoneAnsweredYesTo = "";
+            break;
+        }
+    }
+
+    return everyoneAnsweredYesTo.Length;
+}).Sum();
 
 Console.WriteLine(result);
